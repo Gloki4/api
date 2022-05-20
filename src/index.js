678fc6778186d84a1155393731e790181d1b7dd9
Gloki <gloki95@gmail.com>
2022-05-20 23:11:15 +0200

    test 1 api

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Witaj, Jan Paweł II gwałcił małe dzieci!'));
+app.listen(4000, () => console.log('Serwer nasłuchuje na porcie numer 4000!'));
