382ea567a53b294d141457be3369bab3475c7439
Gloki <gloki95@gmail.com>
2022-05-29 21:55:39 +0200

    koniec rozdziału 3 nodemone

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Witaj, Jan Paweł II gwałcił małe dzieci!'));
-app.listen(4000, () => console.log('Serwer nasłuchuje na porcie numer 4000!'));
+app.get('/', (req, res) => res.send('Pizdy dupa?'));
+app.listen(port, () => console.log('Serwer działa pod adresem http://localhost:%{port}'));
